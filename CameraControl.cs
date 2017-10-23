24c926785c91a4fc24c1578472c378304fd4d89f
DONGQI XU <sdccxdq@gmail.com>
2017-10-23 19:25:24 +0800

    code

---
+++ CameraControl.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CameraControl : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offsetPosition;
+    private Vector3 ballPosition;
+
+    private float initialRotationX;
+
+	// Use this for initialization
+	void Start () {
+		offsetPosition = transform.position - player.transform.position;
+        initialRotationX = transform.rotation.eulerAngles.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float rotationY = PlayerController.rotationVector.y;
+
+        float sinValue = (float)Math.Sin((double)rotationY / 180 * Math.PI);
+        float cosValue = (float)Math.Cos((double)rotationY / 180 * Math.PI);
+
+        float positionX = offsetPosition.x * cosValue + offsetPosition.z * sinValue;
+        float positionZ = offsetPosition.x * -sinValue + offsetPosition.z * cosValue;
+        float positionY = offsetPosition.y;
+        Debug.Log(positionX + " "+ positionY + " " + positionZ);
+
+        // Only consider x, z asix
+		ballPosition = player.transform.position;
+		ballPosition.y = 0;
+
+        transform.position = ballPosition + new Vector3(positionX, positionY, positionZ);
+
+        Vector3 eulerAngles = transform.rotation.eulerAngles;
+
+        float cameraRotation = Input.GetAxis("CameraRotation") * 5;
+        eulerAngles.x = LimitRotation(cameraRotation, eulerAngles.x);
+        transform.rotation = Quaternion.Euler(eulerAngles.x, rotationY, eulerAngles.z);
+	}
+
+    // limit the rotation value
+    float LimitRotation(float rotation, float original)
+    {
+        float tempY = rotation + original;
+        while (tempY > 180)
+        {
+            tempY -= 360;
+        }
+        if (tempY > 90) {
+            tempY = 90;
+        }
+        while (tempY < -180)
+        {
+            tempY += 360;
+        }
+        if (tempY < initialRotationX - 90)
+        {
+            tempY = initialRotationX - 90;
+        }
+        return tempY;
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+    public static Vector3 rotationVector;
+
+	private Rigidbody rb;
+	private int count;
+
+
+	// Use this for initialization
+	void Start () {
+        rotationVector = new Vector3(0, 0, 0);
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Debug.Log ("Update time" + Time.deltaTime);
+	}
+
+	void FixedUpdate() {
+		// Get form keyboard
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		float moveJump = Input.GetAxis ("Jump") * 5;
+        float moveRotation = Input.GetAxis("Rotation") * 10;
+
+        // Rotation Effect
+        float rotationY = UpdateRotation(moveRotation);
+        // Debug.Log("rotationY " + rotationY);
+        float sinValue = (float)Math.Sin((double)rotationY/180*Math.PI);
+        float cosValue = (float)Math.Cos((double)rotationY/180*Math.PI);
+        // Transform
+        float tempX = moveHorizontal * cosValue + moveVertical * sinValue;
+        float tempZ = moveHorizontal * -sinValue + moveVertical * cosValue;
+        // Update
+        moveHorizontal = tempX;
+        moveVertical = tempZ;
+
+        // constrain of height(y axis), no force in the air
+        float yPosition = transform.position.y;
+		if (yPosition > 0.75) {
+			moveJump = 0;
+			moveHorizontal = 0;
+            moveVertical = 0;
+		}
+
+		// constrain of horizontal(x axis), vertical(z asix)
+        // Slow down the speed
+		float coefficient = 0.2f;
+		float xForce = - rb.velocity.x * coefficient;
+		float zForce = - rb.velocity.z * coefficient;
+        // if no force applied, at the ground
+        // What if some surface at the desk
+		if (Math.Abs(moveHorizontal) < 0.05 && yPosition < 0.75)
+			moveHorizontal = xForce;
+		if (Math.Abs(moveVertical) < 0.05 && yPosition < 0.75)
+			moveVertical = zForce;
+
+		Vector3 movement = new Vector3 (moveHorizontal, moveJump, moveVertical);
+		rb.AddForce (movement * speed);
+		// Debug.Log ("FixedUpdate time:" + Time.deltaTime);
+	}
+
+    // Pick up element
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Pick")) {
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+    // Format the Count Text All the time
+	void SetCountText () {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 5)
+			winText.text = "You Win!";			
+	}
+
+    // limit the rotation value
+    float UpdateRotation(float rotation) {
+        float tempY = rotationVector.y + rotation;
+        while (tempY > 180) {
+            tempY -= 360;
+        }
+        while (tempY < - 180) {
+            tempY += 360;
+        }
+        rotationVector.y = tempY;
+        return rotationVector.y;
+    }
+}
